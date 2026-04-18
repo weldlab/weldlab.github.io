bc675e3b3b9ccff81779b184ed43f3d9dd4a563b
Alex <alexanderthestudent@yandex.ru>
2026-04-18 22:23:46 +0300

    fix: copy SVGs

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addGlobalData("layout", "base.njk");
   eleventyConfig.addPassthroughCopy("**/*.css");
+  eleventyConfig.addPassthroughCopy("**/*.svg");
   eleventyConfig.addPassthroughCopy("**/*.pdf");
   eleventyConfig.addPassthroughCopy("**/*.mp4");
 }
