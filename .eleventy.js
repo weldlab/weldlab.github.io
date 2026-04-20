6f52ebb2ebc181071abfd5cf90cc8baba2207287
Alex <alexanderthestudent@yandex.ru>
2026-04-20 09:46:58 +0300

    add results

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addGlobalData("layout", "base.njk");
   eleventyConfig.addPassthroughCopy("**/*.css");
   eleventyConfig.addPassthroughCopy("**/*.svg");
+  eleventyConfig.addPassthroughCopy("**/*.jpg");
+  eleventyConfig.addPassthroughCopy("**/*.png");
   eleventyConfig.addPassthroughCopy("**/*.pdf");
   eleventyConfig.addPassthroughCopy("**/*.mp4");
 }
